b3e9d2f0f843e07197778071c844e53faaaea8a2
Manish Sindur <95417648+manishsindur@users.noreply.github.com>
2025-06-08 08:48:30 +0530

    feat: set up routing with react-router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoutesFromElements, Route } from "react-router-dom";
+import "./index.css";
+import Welcome from "./pages/unauthenticated/Welcome.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  </StrictMode>,
-)
+const router = createBrowserRouter(
+	createRoutesFromElements(<Route path="/" element={<Welcome />} />)
+);
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
