e295540aa9404d817f5a3cb52322685de09012a3
Manish Sindur <95417648+manishsindur@users.noreply.github.com>
2025-07-01 11:17:31 +0530

    Refactor Sidebar component to use dynamic navigation items and improve styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 function Sidebar() {
+	const [activeTab, setActiveTab] = useState("Upload Video");
+	const navItems = [
+		"Upload Video",
+		"Your Videos",
+		"Creator Chat",
+		"Settings",
+		"Logout",
+	];
 	return (
-		<aside className="p-4 h-full shadow-3xl shadow-border rounded-lg">
-			<h1 className="text-white text-2xl">Dashboard Y</h1>
-			<ul>
-				<li className="text-white text-sm">Upload Video</li>
-				<li className="text-white text-sm">Your Videos</li>
-				<li className="text-white text-sm">Creator Chat</li>
-				<li className="text-white text-sm">Settings</li>
-				<li className="text-white text-sm">Logout</li>
+		<aside className="p-4 h-full shadow-3xl shadow-Neutral-600 rounded-lg">
+			<h1 className="text-white text-2xl mb-3">Dashboard Y</h1>
+			<ul className="gap-4">
+				{navItems.map((item) => (
+					<li
+						key={item}
+						onClick={() => setActiveTab(item)}
+						className={`nav-items ${
+							activeTab === item ? "bg-Neutral-600" : "border-l-transparent"
+						}`}
+					>
+						{item}
+					</li>
+				))}
 			</ul>
 		</aside>
 	);
