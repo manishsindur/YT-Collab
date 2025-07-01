ee4fab48ebba223ae497749b8aeeca28b0e84f28
Manish Sindur <95417648+manishsindur@users.noreply.github.com>
2025-07-01 11:29:31 +0530

    Update Sidebar component to replace h1 with div for improved styling consistency

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,8 +11,8 @@ function Sidebar() {
 	];
 	return (
 		<aside className="p-4 h-full shadow-3xl shadow-Neutral-600 rounded-lg">
-			<h1 className="text-white text-2xl mb-3">Dashboard Y</h1>
-			<ul className="gap-4">
+			<div className="text-white text-2xl mb-5">Dashboard Y</div>
+			<ul>
 				{navItems.map((item) => (
 					<li
 						key={item}
